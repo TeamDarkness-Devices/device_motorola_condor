736683383a7f961487a03d52c2b8ba20dd224675
kishpatel1998 <kishannn.p.19@gmail.com>
2017-06-25 09:56:05 +0000

    DND -ify

--- dnd.mk
+++ dnd.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/dnd/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_condor
+PRODUCT_NAME := dnd_condor
 PRODUCT_RELEASE_NAME := MOTO E
